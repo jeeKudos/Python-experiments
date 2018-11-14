d7a3766b2b27bd57dc2873f27ac543128284e71b
jeeKudos <jeenamiria@gmail.com>
2018-11-15 00:07:19 +0530

    Connectedness problem corrected with input taken from user

--- gt3.py
+++ gt3.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+import numpy as np
 def getAllConnectedGroups(graph):
 	alreadySeen= set()
 	result=[]
@@ -13,19 +15,22 @@ def getConnectedGroup(node,alreadySeen):
 	while nodes:
 		node=nodes.pop()
 		alreadySeen.add(node)
-		nodes=nodes or graph[node]-alreadySeen
+		nodes=nodes or set(graph[node])-set(alreadySeen)
 		result.append(node)
 	return result,alreadySeen
 
 #graph={0:{0,1,2,3},1:set(),2:{1,2},3:{3,4,5},4:{3,4,5},5:{4,3},6:{7,8},7:{6},8:{7}}
-graph={}
+graph= defaultdict(list)
+
 n=int(raw_input("Enter number of nodes: "))
 for i in range (1,n+1):
-	a=raw_input("Enter nodes to which node "+str(i)+" is connected: ").split(' ')
-	if a=='':
-		a=set()
-	graph[i-1]=a
-	print a
+	data=map(int,raw_input("Enter nodes to which "+str(i)+" is connected: ").split())
+	if len(data)==0:
+			graph[i]=set()
+	else:
+		for j in data:
+			graph[i].append(j)
+
 result=getAllConnectedGroups(graph)
 for comp in result:
 	print comp
